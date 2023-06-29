aa6a92e17a792f96626624ae1ede3575a8b06040
Jaqueline Gonzaga <jaqueline_gonzaga@hotmail.com>
2023-06-29 09:57:12 -0400

    feat: create module Home

---
+++ src/modules/Home/screens/Initial/index.tsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import {
+    CatImage,
+    CatItem,
+    Container,
+    FavoriteButton,
+    Title,
+    TextInputStyled,
+    Header,
+    IconFavorite,
+    CatList,
+    ViewRow,
+    LogoutButton,
+    IconLogout,
+} from './styles';
+import { useBreedsQuery } from '../../../../services/catApi';
+import { useAuth } from '../../../../contexts/lib/Auth';
+import Modal from '../../../../components/CatModal';
+
+const Home = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [selectedCat, setSelectedCat] = useState(null);
+
+    const { data: cats, isLoading, error, fetchNextPage, hasNextPage } = useBreedsQuery();
+    const { user, updateUserFavorites, logout } = useAuth();
+
+    const handleSearch = (text: string) => {
+        setSearchTerm(text);
+    };
+
+    const handleFavorite = async (catId: string) => {
+        const favorites = user?.favorites || [];
+        const updatedFavorites = [...favorites, catId];
+        updateUserFavorites(updatedFavorites);
+    };
+
+    const handleLoadMore = () => {
+        fetchNextPage();
+    };
+
+    const filteredCats = cats?.pages.flatMap((page) =>
+        page.data.filter((cat: any) =>
+            cat.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+    );
+
+    const handleCatPress = (cat: any) => {
+        setSelectedCat(cat);
+    };
+
+    const handleCloseModal = () => {
+        setSelectedCat(null);
+    };
+
+    const handleLogout = () => {
+        logout();
+    };
+
+    const renderCatItem = ({ item }: { item: any }) => (
+        <CatItem onPress={() => handleCatPress(item)}>
+            {item.image && <CatImage source={{ uri: item.image }} />}
+            <Title>{item.name}</Title>
+            <FavoriteButton onPress={() => handleFavorite(item.id)}>
+                <IconFavorite name="heart" />
+            </FavoriteButton>
+        </CatItem>
+    );
+
+    if (isLoading) {
+        return (
+            <Container>
+                <Title>Carregando...</Title>
+            </Container>
+        );
+    }
+
+    if (error) {
+        return (
+            <Container>
+                <Title>Error ao carregar espécies</Title>
+            </Container>
+        );
+    }
+
+    return (
+        <Container>
+            <Header>
+                <ViewRow>
+                    <Title>Olá, {user?.fullName}</Title>
+                    <LogoutButton onPress={handleLogout}>
+                        <IconLogout name="power" />
+                    </LogoutButton>
+                </ViewRow>
+                <TextInputStyled
+                    placeholder="Pesquisar por espécie"
+                    value={searchTerm}
+                    onChangeText={handleSearch}
+                />
+            </Header>
+            <CatList
+                data={filteredCats}
+                renderItem={renderCatItem}
+                keyExtractor={(item) => item.id}
+                numColumns={2}
+                onEndReached={handleLoadMore}
+                onEndReachedThreshold={0.1}
+                ListFooterComponent={hasNextPage ? <Title>Carregando mais...</Title> : null}
+                showsVerticalScrollIndicator={false}
+            />
+            {selectedCat && (
+                <Modal cat={selectedCat} onClose={handleCloseModal} />
+            )}
+        </Container>
+    );
+};
+
+export default Home;
